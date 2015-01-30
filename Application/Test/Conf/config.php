b216a607ca837a4fea1c62bc10c7734a355d6624
pysnow530 <pysnow530@163.com>
2015-01-30 11:24:35 +0800

    添加测试数据库配置

--- Application/Test/Conf/config.php
+++ Application/Test/Conf/config.php
@@ -2,8 +2,18 @@
 return array(
 
     // 添加Library文件夹到命名空间
-    'AUTOLOAD_NAMESPACE' => array(
-        'Library' => APP_PATH . '../Library',
+    "AUTOLOAD_NAMESPACE" => array(
+        "Library" => APP_PATH . "../Library",
+    ),
+
+    // 测试数据库配置
+    "DB_CONFIG_UNIT_TEST" => array(
+        "db_type"   => "mysql",
+        "db_user"   => "root",
+        "db_pwd"    => "123456",
+        "db_host"   => "localhost",
+        "db_port"   => "3306",
+        "db_name"   => "db_demo_unit_test",
     ),
 
 );
